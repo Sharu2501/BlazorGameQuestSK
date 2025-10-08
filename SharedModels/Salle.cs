51540b1e8e3e555fd315e9d61ec3ed7921136b37
Sasikumar Sahkana <shakana2604@gmail.com>
2025-10-08 13:32:01 +0200

    Connexion Joueur + Accueil

--- SharedModels/Salle.cs
+++ SharedModels/Salle.cs
@@ -7,18 +7,20 @@ namespace SharedModels
         DIFFICILE,
         EXTREME
     }
+
     public class Salle
     {
-        private int Id { get; set; }
-        private string Nom { get; set; } = null!;
-        private int Niveau { get; set; }
-        private string Description { get; set; } = null!;
-        private Monster? Monstre { get; set; }
-        private bool EstExploree { get; set; }
-        private int ExperienceGagnee { get; set; }
-        private int OrGagne { get; set; }
-        private NiveauDifficulteEnum NiveauDifficulte { get; set; }
-        private Joueur Joueur { get; set; } = null!;
+        public int Id { get; set; }
+        public string Nom { get; set; } = null!;
+        public int Niveau { get; set; }
+        public string Description { get; set; } = null!;
+        public Monster? Monstre { get; set; }
+        public bool EstExploree { get; set; }
+        public int ExperienceGagnee { get; set; }
+        public int OrGagne { get; set; }
+        public NiveauDifficulteEnum NiveauDifficulte { get; set; }
+        public Joueur Joueur { get; set; } = null!;
+
         public Salle()
         {
             Id = 0;
@@ -32,7 +34,10 @@ namespace SharedModels
             OrGagne = 0;
             NiveauDifficulte = NiveauDifficulteEnum.FACILE;
         }
-        public Salle(int id, string nom, int niveau, string description, Monster? monstre, bool estExploree, int experienceGagnee, int orGagne, NiveauDifficulteEnum niveauDifficulte, Joueur joueur)
+
+        public Salle(int id, string nom, int niveau, string description, Monster? monstre,
+                     bool estExploree, int experienceGagnee, int orGagne,
+                     NiveauDifficulteEnum niveauDifficulte, Joueur joueur)
         {
             Id = id;
             Nom = nom;
@@ -45,10 +50,12 @@ namespace SharedModels
             NiveauDifficulte = niveauDifficulte;
             Joueur = joueur;
         }
+
         public void Explorer()
         {
             EstExploree = true;
         }
+
         public override string ToString()
         {
             return $"Salle [Id={Id}, Nom={Nom}, Niveau={Niveau}, Description={Description}, Monstre={Monstre}, EstExploree={EstExploree}, ExperienceGagnee={ExperienceGagnee}, OrGagne={OrGagne}, NiveauDifficulte={NiveauDifficulte}, Joueur={Joueur}]";
